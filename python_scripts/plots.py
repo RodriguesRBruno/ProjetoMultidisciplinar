88918fd6c903b089c60953b9b6e817cb7f0a2bc0
Bruno <brunoresenderodrigues@gmail.com>
2022-11-09 21:38:01 +0000

    Reorganizando scripts
    Movendo algumas funções úteis para scripts na pasta python_scripts, assim fica mais fácil de reutilizar depois

---
+++ python_scripts/plots.py
@@ -0,0 +1,27 @@
+"""
+Arquivo para scripts de plots
+"""
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def plot_cloud(words_string):
+    """
+    Função para plotar nuvem de palavras
+    """
+    nuvem_palavras = WordCloud(width= 1000, height= 800, max_font_size = 110, collocations= False).generate(words_string)
+    plt.figure(figsize=(12,8))
+    plt.imshow(nuvem_palavras, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    
+def plot_word_counts(tokens, n=10):
+    """
+    Função para plotar gráfico com contagens das 'n' palavras mais comuns presentes nos 'tokens'
+    """
+    frequency = nltk.FreqDist(tokens)
+    df_frequency = pd.DataFrame({"Word": list(frequency.keys()),
+                                 "Frequency": list(frequency.values())})
+    plt.figure(figsize=(16,8))
+    ax = sns.barplot(data = df_frequency.sort_values(by=['Frequency'], ascending=False).head(n), x = "Word", y = "Frequency")
+    ax.set(ylabel = "Count")
+    plt.show()
---
+++ python_scripts/save_load.py
@@ -0,0 +1,42 @@
+"""
+Arquivo para scripts referentes a salvar/carregar dados
+"""
+
+import boto3
+import pandas as pd
+
+BUCKET_BRUTOS = 'projetointerdisciplinardadosbrutos'
+BUCKET_PROCESSADOS = 'projetointerdisciplinardadosprocessados'
+
+def __get_bucket(processado=False):
+    if processado:
+        return BUCKET_PROCESSADOS
+    else:
+        return BUCKET_BRUTOS
+
+    
+def load_df_from_bucket(file_name, processado=False):
+    """
+    Função auxiliar para ler csv de buckets do projeto.
+    Se processado for True, lê o bucket de dados processados; 
+    caso contrário, lê bucket de dados brutos.
+    """
+    bucket = __get_bucket(processado)
+    return pd.read_csv(f's3://{bucket}/{file_name}')
+
+
+def save_to_s3_bucket(file_name, processado=False):
+    """
+    Função auxiliar para fazer upload de um arquivo para bucket no S3.
+    Se processado for True, salva no bucket de dados processados; 
+    caso contrário, salva no bucket de dados brutos.
+    """
+    bucket = __get_bucket(processado)
+    s3 = boto3.resource('s3')
+    with open(file_name, 'rb') as f:
+        s3.Bucket(bucket).put_object(Key=file_name, Body=f)
+        
+
+def save_df_to_s3_bucket(df, file_name, processado=False):
+    bucket = __get_bucket(processado)
+    df.to_csv(f's3://{bucket}/{file_name}')
