5f05c2d663bb26a625f20f4099ec9904f7bba378
Bruno <brunoresenderodrigues@gmail.com>
2022-11-10 13:25:11 +0000

    Ajustes para vários buckets
    Ajustes para permitir salvar/baixar dados de mais do que 2 buckets

--- python_scripts/save_load.py
+++ python_scripts/save_load.py
@@ -7,36 +7,38 @@ import pandas as pd
 
 BUCKET_BRUTOS = 'projetointerdisciplinardadosbrutos'
 BUCKET_PROCESSADOS = 'projetointerdisciplinardadosprocessados'
+BUCKET_MODEL = 'projetointerdisciplinartreinoteste'
 
-def __get_bucket(processado=False):
-    if processado:
+def __get_bucket(tipo='bruto'):
+    if tipo=='processado':
         return BUCKET_PROCESSADOS
-    else:
+    elif tipo == 'bruto':
         return BUCKET_BRUTOS
-
+    elif tipo == 'model':
+        return BUCKET_MODEL
     
-def load_df_from_bucket(file_name, processado=False):
+def load_df_from_bucket(file_name, tipo='bruto'):
     """
     Função auxiliar para ler csv de buckets do projeto.
     Se processado for True, lê o bucket de dados processados; 
     caso contrário, lê bucket de dados brutos.
     """
-    bucket = __get_bucket(processado)
+    bucket = __get_bucket(tipo)
     return pd.read_csv(f's3://{bucket}/{file_name}')
 
 
-def save_to_s3_bucket(file_name, processado=False):
+def save_to_s3_bucket(file_name, tipo='bruto'):
     """
     Função auxiliar para fazer upload de um arquivo para bucket no S3.
     Se processado for True, salva no bucket de dados processados; 
     caso contrário, salva no bucket de dados brutos.
     """
-    bucket = __get_bucket(processado)
+    bucket = __get_bucket(tipo)
     s3 = boto3.resource('s3')
     with open(file_name, 'rb') as f:
         s3.Bucket(bucket).put_object(Key=file_name, Body=f)
         
 
-def save_df_to_s3_bucket(df, file_name, processado=False):
-    bucket = __get_bucket(processado)
+def save_df_to_s3_bucket(df, file_name, tipo='bruto'):
+    bucket = __get_bucket(tipo)
     df.to_csv(f's3://{bucket}/{file_name}', encoding='utf-8', index=False)
