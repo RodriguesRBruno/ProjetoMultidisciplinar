491146daab3da06cceb0f25842376268acb054b2
Bruno <brunoresenderodrigues@gmail.com>
2022-11-19 13:19:30 +0000

    Correções para nova estrutura de pastas

---
+++ notebooks/python_scripts/.ipynb_checkpoints/modelling-checkpoint.py
@@ -0,0 +1,87 @@
+from sklearn.model_selection import train_test_split
+import sagemaker
+import s3fs
+import boto3
+from python_scripts.save_load import BUCKET_MODEL
+import pandas as pd
+
+def create_train_validation_test_sets(model_df, stratify_col, test_size=0.2, random_state=None, validation_size=0.5):
+    """
+    Função auxiliar para gerar sets de treinamento, validação e teste.
+    Inicialmente separa os dados de treinamento e teste+validação conforme proporção dada em test_size.
+    Em seguida, o set de teste+validação é separado na metade para definir sets de teste e validação individualmente.
+    """      
+    train, test_validate = train_test_split(model_df, test_size=test_size, random_state=random_state,
+                                           stratify=model_df[stratify_col])
+    test, validate = train_test_split(test_validate, test_size=validation_size, random_state=random_state,
+                                      stratify=test_validate[stratify_col])
+    
+    return train, test, validate
+
+
+def setup_model(model_name, base_image='xgboost', instance_count=1, instance_type='ml.m4.xlarge',
+                hyperparams=None):
+    container = sagemaker.image_uris.retrieve('xgboost',boto3.Session().region_name,'1.5-1')
+    
+    if hyperparams is None:
+        # Valores padrão de um lab
+        hyperparams={"num_round":"42",
+                     "eval_metric": "map",
+                     "objective": "binary:logistic"}
+
+    s3_output_location=f"s3://{BUCKET_MODEL}/output/{model_name}"
+
+    xgb_model=sagemaker.estimator.Estimator(container,
+                                            sagemaker.get_execution_role(),
+                                            instance_count=instance_count,
+                                            instance_type=instance_type,
+                                            output_path=s3_output_location,
+                                            hyperparameters=hyperparams,
+                                            sagemaker_session=sagemaker.Session())
+
+    train_channel = sagemaker.inputs.TrainingInput(
+        "s3://{}/{}/{}".format(BUCKET_MODEL,'train',f'{model_name}.libsvm'), content_type='libsvm')
+
+    validate_channel = sagemaker.inputs.TrainingInput(
+        "s3://{}/{}/{}".format(BUCKET_MODEL,'validate',f'{model_name}.libsvm'), content_type='libsvm')
+
+    data_channels = {'train': train_channel, 'validation': validate_channel}
+    
+    return xgb_model, data_channels
+
+
+def make_prediction(predictor, model_name, threshold=0.5):
+    fs = s3fs.S3FileSystem()
+    s3_path = f's3://projetointerdisciplinartreinoteste/test/{model_name}.libsvm'
+    
+    with fs.open(s3_path) as libsvm_file:
+        y_pred = predictor.predict(libsvm_file)
+        
+    y_pred_list = [1 if float(x) >= threshold else 0 for x in y_pred.decode('utf-8').split('\n') if x != '']
+    return y_pred_list
+
+def make_prediction_recordio(predictor, model_name, threshold=0.5):
+    fs = s3fs.S3FileSystem()
+    s3_path = f's3://projetointerdisciplinartreinoteste/test/{model_name}.protobuf'
+    
+    with fs.open(s3_path) as protobuf_file:
+        y_pred = predictor.predict(protobuf_file)
+        
+    y_pred_list = [1 if float(x) >= threshold else 0 for x in y_pred.decode('utf-8').split('\n') if x != '']
+    return y_pred_list
+
+def load_batch_results(model_name):
+    """
+    Retorna um DataFrame com resultados de uma previsão feita via batch
+    """
+    fs = s3fs.S3FileSystem()
+
+    records_path = f"s3://{BUCKET_MODEL}/test/batch-out/{model_name}.protobuf.out"
+
+    with fs.open(records_path) as f: 
+        res_str = f.read().decode('utf-8')
+
+    res_list = [eval(a) for a in res_str.split('\n')[:-1]]
+    df_pred = pd.DataFrame(res_list)
+    
+    return df_pred
---
+++ notebooks/python_scripts/.ipynb_checkpoints/processing-checkpoint.py
@@ -0,0 +1,92 @@
+"""
+Arquivo para scripts de processamento de dados
+"""
+
+import numpy as np
+import pandas as pd
+import nltk
+import stanza
+nltk.download('all', quiet=True)
+stanza.download('pt', verbose=False)
+
+def tokenize_text_and_get_stats(text, nlp_pipeline, stopword_list=None):
+    """
+    Função para tokenizar um texto. 
+    Texto deve etar na coluna 'text'
+    Retorna uma tupla com 4 valores:
+    - Word_list (tokens de palavras, excluindo stopwords)
+    - Lemma_list (tokens de lemmas das palavras, desde que a palavra original não seja stopword)
+    - Tamanho médio de frases do texto (em palavras, excluindo stopwords e pontuação)
+    - Tamanho médio de palavras no texto (excluindo stopwords e pontuação)
+    """
+    if stopword_list is None:
+        stopword_list = []
+    
+    processed = nlp_pipeline(text)
+    word_list = []
+    lemma_list = []
+
+    word_avg_lengths = []
+
+    sentence_lengths = []
+    for sent in processed.sentences:
+        local_word_list = []
+        local_word_lengths = []
+        for word in sent.words:
+            if word.text.lower() in stopword_list or word.pos == 'PUNCT':
+                continue
+            local_word_list.append(word.text)
+            lemma_list.append(word.lemma)
+            local_word_lengths.append(len(word.text))
+
+        if local_word_list:
+            sentence_lengths.append(len(local_word_list))
+
+            word_list.extend(local_word_list)
+            avg_word_len = np.mean(local_word_lengths)
+            word_avg_lengths.append(avg_word_len)
+    
+    return word_list, lemma_list, np.mean(sentence_lengths), np.mean(word_avg_lengths)
+
+
+def tokenize_df_row_and_get_stats(df_row, nlp_pipeline, stopword_list=None, n_print=500):
+    """
+    Função para tokenizar um texto localizado em uma linha de um DataFrame. 
+    Texto deve etar na coluna 'text'
+    Retorna uma tupla com 4 valores:
+    - Word_list (tokens de palavras, excluindo stopwords)
+    - Lemma_list (tokens de lemmas das palavras, desde que a palavra original não seja stopword)
+    - Tamanho médio de frases do texto (em palavras, excluindo stopwords e pontuação)
+    - Tamanho médio de palavras no texto (excluindo stopwords e pontuação)
+    """
+    
+    if n_print is not None and df_row.name % n_print == 0:
+        print(f'Processando linhas {df_row.name} a {df_row.name+n_print-1}...')
+    text = df_row['text']
+    return tokenize_text_and_get_stats(text, nlp_pipeline, stopword_list)
+
+
+def create_tokenized_df(base_df, remove_nao=True):
+    """
+    Função para criar colunas no DataFrame referentes à tokenização
+    """
+    if 'text' not in base_df.columns:
+        return base_df
+    
+    df = base_df.copy()
+    nlp = stanza.Pipeline('pt', processors='tokenize,mwt,pos,lemma')
+    stopwords = nltk.corpus.stopwords.words('portuguese')
+    if remove_nao:
+        stopwords.remove('não')
+    df[['words', 'lemmas', 'avg_sent_len', 'avg_word_len']] = df.apply(tokenize_df_row_and_get_stats, axis=1,
+                                                                       result_type='expand',
+                                                                       stopword_list=stopwords,
+                                                                       nlp_pipeline=nlp)
+    return df
+
+def evaluate_words_lemmas(df_row):
+    """
+    Função auxiliar para converter strings de words/lemmas de volta em listas
+    Utilizado pois no csv tudo vira string
+    """
+    return eval(df_row['words']), eval(df_row['lemmas'])
